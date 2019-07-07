350ba1769ae732e0ae8b2471364c214e728622c3
George Higbie <ghigbie@monigle.com>
2019-07-07 15:31:50 -0600

    Add conditional statements and rendering

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -7,5 +7,41 @@ class WeatherModel {
   Future <dynamic> getLocationWeather() async{
     Location location = Location();
     await location.getCurrentLocation();
+
+    NetworkHelper networkHelper =NetworkHelper(url: 
+    '$kOpenWeatherBaseURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPIKey&units=imperial');
+  }
+
+
+  String getDoggiePicture(int condition){
+    if (condition < 300){
+      return 'slightly sad dog';
+    } else if (condition < 400) {
+      return 'more sad dog';
+    } else if (condition < 600) {
+      return 'very sad dog';
+    } else if (condition < 700) {
+      return 'dog in sweater';
+    } else if (condition < 800) {
+      return 'dog in t shirt';
+    } else if (condition == 800) {
+      return '☀dog in sunglasses and happy';
+    } else if (condition <= 804) {
+      return 'happy dog';
+    } else {
+      return 'neutral dog';
+    }
+  }
+
+  String getMessage(int temp){
+    if (temp >= 77 ) {
+      return 'It\'s time to give your doggie icecream!';
+    } else if (temp >= 63) {
+      return 'Take off your doggies\'s sweater';
+    } else if (temp < 50) {
+      return 'Put sweater or jacket on your doggie!';
+    } else {
+      return 'Bring a doggie 🧥 just in case';
+    }
   }
 }
