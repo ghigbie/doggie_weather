f671d0504c4493b911dda081f2d39d6504921808
George Higbie <ghigbie@monigle.com>
2019-07-07 15:14:29 -0600

    Add constants for open weather api

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -2,3 +2,10 @@ import 'package:doggie_weather/services/location.dart';
 import 'networking.dart';
 import 'package:doggie_weather/utilities/constants.dart';
 import 'package:doggie_weather/utilities/hidden_constants.dart';
+
+class WeatherModel {
+  Future <dynamic> getLocationWeather() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+  }
+}
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,1 +1,2 @@
 const kAppTitle = 'Doggie Weather';
+const kOpenWeatherBaseURL = 'https://api.openweathermap.org/data/2.5/weather';
