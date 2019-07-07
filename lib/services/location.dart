29ba5420f4bd71c39b9cd846bf6928568ceaa80d
George Higbie <ghigbie@monigle.com>
2019-07-07 15:08:05 -0600

    Add network helper method

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,11 +5,14 @@ class Location {
    double longitude;
    String errorMessage;
 
-   Future<void> getCurrentLocation(){
+   Future<void> getCurrentLocation() async{
      try{
-
+       Position position  = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+       latitude = position.latitude;
+       longitude = position.longitude;
      }catch(e){
-       
+       print('Exception: $e');
+       errorMessage = 'Sorry, your location could not be determined. Please try again later.';
      }
    }
 
