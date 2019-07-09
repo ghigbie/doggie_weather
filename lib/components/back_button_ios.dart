2beafef844766e9a9bcbb3244ffd6749182661a9
George Higbie <ghigbie@monigle.com>
2019-07-08 20:53:37 -0600

    Add new constants

--- lib/components/back_button_ios.dart
+++ lib/components/back_button_ios.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class BackButton extends StatelessWidget {
+class BackButtonIOS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton(
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 const kAppTitle = 'Doggie Weather';
+
 const kLoadingPageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
   fontSize: 24.0,
   color: Color(0xff242424),
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white),
+                    hintText: 'Enter City Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide.none,
+                    ),
+                  );
+
 const kOpenWeatherBaseURL = 'https://api.openweathermap.org/data/2.5/weather';
